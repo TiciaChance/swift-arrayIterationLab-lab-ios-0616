f7aaacb942ebb081da2f89ec78811ab72700232f
TiciaChance <laticia.chance@flatironschool.com>
2016-07-07 13:07:03 -0400

    done

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -8,16 +8,14 @@
  * [Array](https://github.com/learn-co-curriculum/swift-array-readme)
  * [Array Iteration](https://github.com/learn-co-curriculum/swift-arrayIteration-readme)
  
-
+ 
  */
 /*: question1
  ### 1. You're building an app and want to store all of the ingredients added to a cart. How would you represent this cart in code? The ingredients are "Chips", "Salsa", "Guacamole", "Red wine". Explicitly mark the content of the cart as strings.
  */
 // write your code here
 
-
-
-
+var shoppingCart = ["Chips", "Salsa", "Guacamole", "Red wine"]
 
 
 /*: question2
@@ -26,18 +24,19 @@
 // write your code here
 
 
+var numberOfItemsInCart: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-
-
-
-
+//var numbers = shoppingCart.indices
+//var numbers = [1...9]
 /*: question3
  ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible.
  */
 // write your code here
 
 
-
+for i in numberOfItemsInCart {
+    print(i)
+}
 
 
 
@@ -47,10 +46,9 @@
  */
 // write your code here
 
-
-
-
-
+for item in shoppingCart {
+    print("I need this item for lunch: \(item)")
+}
 
 
 /*: question5
@@ -58,8 +56,17 @@
  */
 // write your code here
 
+//shoppingCart
 
 
+func printItems(newArray: Array<String>) {
+    
+    for item in newArray {
+        print(item)
+    }
+}
+
+printItems(shoppingCart)
 
 
 
@@ -70,20 +77,41 @@
 // write your code here
 
 
+func nameAndGreetings (name:Array<String>) {
+    
+    for personName in name {
+        if personName == "Michael" {
+            print("Top of the morning, \(personName)")
+        } else {
+            print("Good morning, \(personName)")
+        }
+    }
+}
 
 
-
+let listOfNames = ["Joe", "Jim", "Bob", "Michael"]
+nameAndGreetings(listOfNames)
 
 /*: question7
  ### 7. Create a function that takes an array of Ints (40, 60, 50, 52, 59, 13, 90, 100, 5, 52, 51, 49) and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
  */
 // write your code here
 
+func allInts (theInts: [Int]) -> [Int] {
+    
+    var emptyArray: [Int] = []
+    
+    for one in theInts {
+        if one < 50 {
+            emptyArray.append(one)
+        }
+    }
+    return emptyArray
+}
 
+var arrayOfInts = [40, 60, 50, 52, 59, 13, 90, 100, 5, 52, 51, 49]
 
-
-
-
+allInts(arrayOfInts)
 
 
 //: Check here on the solution branch for a link to the solution file
